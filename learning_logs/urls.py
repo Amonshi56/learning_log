5d826e59bbcb3d2ae2a358714d0c5a927d4dd981
Stanislav Krikunov <krikunov.stas@yandex.ru>
2024-02-07 20:10:18 +0300

    Темы и записи
    Создан шаблон страницы и на основании его добавлены страницы для списка тем и списка записей для каждой темы.

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -1,10 +1,17 @@
 '''Определяет схемы URL для learning_logs.'''
 
+
 from django.urls import path 
+
 
 from . import views
 app_name = 'learning_logs'
-urlpatterns = [
+jls_extract_var = [
     # Домашняя страница
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    # Страница со списком всех тем
+    path('topics/', views.topics, name='topics'),
+    # Страница с подробной информацией по теме
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
+urlpatterns = jls_extract_var
